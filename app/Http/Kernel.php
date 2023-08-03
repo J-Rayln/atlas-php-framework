42f64de9a9e52ccf0b9cc74bd3f8765d4f246f3d
Michael Morales <mike@michaelmorales.org>
2023-08-03 09:23:17 -0700

    Create initial project directory structure

---
+++ app/Http/Kernel.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Atlas\App\Http;
+
+/**
+ * Class Kernel
+ *
+ * @package Atlas\App;
+ */
+class Kernel
+{
+
+    public function __construct()
+    {
+        echo 'Kernel instantiated';
+    }
+}
--- public/index.php
+++ public/index.php
@@ -1,1 +1,9 @@
 <?php
+
+use Atlas\App\Http\Kernel;
+
+require dirname(__DIR__) . '/vendor/autoload.php';
+$dotenv = Dotenv\Dotenv::createImmutable(dirname(__DIR__));
+$dotenv->load();
+
+(new Kernel());
